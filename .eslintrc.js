6b16a1e70b885fb9b3285f9c19a4d909360520be
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2022-11-24 12:46:07 +0900

    add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,86 @@ module.exports = {
     sourceType: 'module',
     project: ['./tsconfig.json'],
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'unused-imports',
+    'prettier',
+  ],
   rules: {
+    //
+    // Base
+    //
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    camelcase: 'error',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    //
+    //  Plugin React
+    //
+    'react/display-name': 0,
+    'react/jsx-boolean-value': 0,
+    'react/jsx-no-comment-textnodes': 1,
+    'react/jsx-no-duplicate-props': 2,
+    'react/jsx-no-undef': 2,
+    'react/jsx-sort-props': 0,
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
+    'react/jsx-key': [1, { checkFragmentShorthand: true }],
+    'react/no-did-mount-set-state': 1,
+    'react/no-did-update-set-state': 1,
+    'react/no-multi-comp': 0,
+    'react/no-string-refs': 1,
+    'react/no-unknown-property': 2,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 1,
+    'react/self-closing-comp': 1,
+    'react/wrap-multilines': 0,
+    //
+    //  Plugin react-hooks
+    //
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    //
+    //  Plugin typescript-eslint
+    //
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        varsIgnorePattern: '^_$',
+      },
+    ],
+    //
+    //  Plugin unused-imports
+    //
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    //
+    //  Plugin import
+    //
     'import/order': [
       'error',
       {
