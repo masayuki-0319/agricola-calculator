532145ce9bac97d6a5083618de397f09bd74d19d
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2022-11-25 10:30:25 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
     'prettier',
   ],
@@ -137,11 +137,7 @@ module.exports = {
   },
   settings: {
     react: {
-      createClass: 'createReactClass', // Regex for Component Factory to use,
-      pragma: 'React', // Pragma to use, default to "React"
-      fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
       version: 'detect', // React version. "detect" automatically picks the version you have installed.
-      flowVersion: '0.53', // Flow version
     },
   },
 };
