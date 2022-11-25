98b65a7b748bde5b226e27eaad262749931305f5
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2022-11-25 23:28:07 +0900

    remove tamagui

--- tamagui.config.js
+++
@@ -1,9 +0,0 @@
-import { createTamagui } from '@tamagui/core';
-import { shorthands } from '@tamagui/shorthands';
-import { themes, tokens } from '@tamagui/theme-base';
-
-export default createTamagui({
-  themes,
-  tokens,
-  shorthands,
-});
