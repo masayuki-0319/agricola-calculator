966f438c98811b911429cd5fb7b0aacda1942db8
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2022-12-11 11:41:06 +0900

    add comment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,6 +7,11 @@ import { ProductImage } from '../../assets';
 
 type Props = {};
 
+/**
+ * @FIXME
+ * Footer を画面の最下部に接地させること
+ * @See https://scrapbox.io/momomo/ReactNative_%E3%81%A7%E7%94%BB%E5%83%8F%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B
+ */
 export const Footer: React.FC<Props> = () => {
   return (
     <Box flex={1} safeAreaTop>
